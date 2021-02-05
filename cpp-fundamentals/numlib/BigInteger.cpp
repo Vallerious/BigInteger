e2d5b3112dc7c12465ce70b0f60e42099a94d41a
Vallerious <valericfbg@gmail.com>
2021-02-05 15:28:06 +0200

    refactoring and performance benchmarking

--- cpp-fundamentals/numlib/BigInteger.cpp
+++ cpp-fundamentals/numlib/BigInteger.cpp
@@ -14,7 +14,7 @@ private:
     enum OP { ADD, SUB, MUL, DEL };
     std::string zero = "0";
 
-    bool isValid(std::string digits) {
+    bool isValid(const std::string& digits) {
         if (digits.empty()) {
             return false;
         }
@@ -34,89 +34,93 @@ private:
         return true;
     }
 
-    std::string eval(std::string a, std::string b, BigInteger::OP operation) const {
-        bool AIsNegative = _isNeg(a);
-        bool BIsNegative = _isNeg(b);
+    BigInteger eval(const BigInteger& a, const BigInteger& b, BigInteger::OP operation) const {
+        bool AIsNegative = a.isNegative();
+        bool BIsNegative = b.isNegative();
 
         // if x = 0 or y = 0 or (x = 0 and y = 0)
-        if (zero == a) {
-            return zero == b ? zero : b;
-        } else if (zero == b) {
-            return zero == a ? zero : a;
+        if (a.isZero()) {
+            return b.isZero() ? zero : b;
+        } else if (b.isZero()) {
+            return a.isZero() ? zero : a;
         }
 
         // _add a,b when |a| = |b| and a < 0 and b > 0 or a > 0 and b < 0
         if (((AIsNegative && !BIsNegative) || (!AIsNegative && BIsNegative)) &&
-            _abs(a).compare(_abs(b)) == 0 && operation == BigInteger::OP::ADD) {
-            return "0";
+            a.abs() == b.abs() && operation == BigInteger::OP::ADD) {
+            return BigInteger("0");
         }
 
         if (operation == BigInteger::OP::ADD) {
             // -x + (-y) = -x - y
             if (AIsNegative && BIsNegative) {
-                return _negate(eval(_abs(a), _abs(b), BigInteger::OP::ADD));
+                return eval(a.abs(), b.abs(), BigInteger::OP::ADD).negate();
             }
-            // x + (-y) = x - y
+                // x + (-y) = x - y
             else if (!AIsNegative && BIsNegative) {
-                return eval(a, _abs(b), BigInteger::OP::SUB);
+                return eval(a, b.abs(), BigInteger::OP::SUB);
             }
 
-            // -x + y = y - x
+                // -x + y = y - x
             else if (AIsNegative && !BIsNegative) {
-                return _subtract(_abs(b), _abs(a));
+                return _subtract(b.abs(), a.abs());
             }
 
-            // x + y
+                // x + y
             else if (!AIsNegative && !BIsNegative) {
                 return _add(a, b);
             }
         } else if (operation == BigInteger::OP::SUB) {
             // -x - y
             if (AIsNegative && !BIsNegative) {
-                return _negate(eval(_abs(a), _abs(b), BigInteger::OP::ADD));
+                return eval(a.abs(), b.abs(), BigInteger::OP::ADD).negate();
             }
-            // x - y
+                // x - y
             else if (!AIsNegative && !BIsNegative) {
+                if (a == b) {
+                    return BigInteger("0");
+                }
+
                 return _subtract(a, b);
             }
 
-            // -x - (-y) = -x + y
+                // -x - (-y) = -x + y
             else if (AIsNegative && BIsNegative) {
-                return eval(a, _abs(b), BigInteger::OP::ADD);
+                return eval(a, b.abs(), BigInteger::OP::ADD);
             }
 
-            // x - (-y) = x + y
+                // x - (-y) = x + y
             else if (!AIsNegative && BIsNegative) {
-                return eval(a, _abs(b), BigInteger::OP::ADD);
+                return eval(a, b.abs(), BigInteger::OP::ADD);
             }
         }
 
-        return "";
+        return BigInteger("0");
     }
 
-    std::string _negate(std::string a) const {
-        if (a.at(0) == '-') {
+    std::string _negate(const std::string& a) const {
+        if (_isNeg(a)) {
             return a;
         }
 
         return "-" + a;
     }
 
-    std::string _abs(std::string a) const {
-        if (a.at(0) == '-') {
+    std::string _abs(const std::string& a) const {
+        if (_isNeg(a)) {
             return a.substr(1);
         }
 
         return a;
     }
 
-    bool _isNeg(std::string a) const {
+    bool _isNeg(const std::string& a) const {
         char firstCharInA = a.at(0);
 
         return firstCharInA == '-';
     }
 
-    int _compareStrNumber(std::string a, std::string b) const {
+    int _compareStrNumber(const std::string& a, const std::string& b) const {
         bool AIsNegative = _isNeg(a);
         bool BIsNegative = _isNeg(b);
 
@@ -145,13 +149,13 @@ private:
         return a.compare(b);
     }
 
-    std::string _max(std::string a, std::string b) const {
+    std::string _max(const std::string& a,const std::string& b) const {
         return _compareStrNumber(a, b) < 0 ? b : a;
     }
 
-    std::string _add(std::string a, std::string b) const {
-        int lenA = a.size();
-        int lenB = b.size();
+    BigInteger _add(const BigInteger& a, const BigInteger& b) const {
+        int lenA = a.digitsCount;
+        int lenB = b.digitsCount;
         int idxA = lenA - 1;
         int idxB = lenB - 1;
         std::string sumStr;
@@ -159,8 +163,8 @@ private:
         short carry = 0;
 
         while (idxA >= 0 || idxB >= 0) {
-            short ac = idxA < 0 ? 0 : a.at(idxA) - '0';
-            short bc = idxB < 0 ? 0 : b.at(idxB) - '0';
+            short ac = idxA < 0 ? 0 : a.digits.at(idxA) - '0';
+            short bc = idxB < 0 ? 0 : b.digits.at(idxB) - '0';
 
             short digitSum = ac + bc + carry;
 
@@ -171,6 +175,7 @@ private:
                 carry = 0;
             }
 
+            // TODO: this is not great...create a string class that is optimized for prepend.
             sumStr = std::to_string(digitSum) + sumStr;
 
             idxA -= 1;
@@ -181,16 +186,16 @@ private:
             sumStr = "1" + sumStr;
         }
 
-        return sumStr;
+        return BigInteger(sumStr);
     }
 
-    std::string _subtract(std::string a, std::string b) const {
+    BigInteger _subtract(const BigInteger& a, const BigInteger& b) const {
         // make so that a > b
-        std::string subtractee = _max(a, b);
-        std::string subtractor = a == subtractee ? b : a;
+        BigInteger subtractee = a.max(b);
+        BigInteger subtractor = a == subtractee ? b : a;
 
-        int lenA = subtractee.size();
-        int lenB = subtractor.size();
+        int lenA = subtractee.digitsCount;
+        int lenB = subtractor.digitsCount;
         int idxA = lenA - 1;
         int idxB = lenB - 1;
         std::string subResult;
@@ -198,8 +203,8 @@ private:
         short carry = 0;
 
         while (idxA >= 0 || idxB >= 0) {
-            short ac = idxA < 0 ? 0 : subtractee.at(idxA) - '0';
-            short bc = idxB < 0 ? 0 : subtractor.at(idxB) - '0';
+            short ac = idxA < 0 ? 0 : subtractee.digits.at(idxA) - '0';
+            short bc = idxB < 0 ? 0 : subtractor.digits.at(idxB) - '0';
 
             short digitSub = ac - bc - carry;
 
@@ -221,7 +226,7 @@ private:
         return a < b ? _negate(subResult) : subResult;
     }
 public:
-    BigInteger(std::string digits): digits(digits) {
+    BigInteger(const std::string& digits): digits(digits) {
         if (!isValid(digits)) {
             throw std::invalid_argument("parameter cannot be empty or negative and must be a valid integer");
         }
@@ -232,7 +237,23 @@ public:
         return digits;
     }
 
-    BigInteger max(BigInteger& b) const {
+    bool isNegative() const {
+        return _isNeg(digits);
+    }
+
+    bool isZero() const {
+        return digits == zero;
+    }
+
+    BigInteger negate() const {
+        return BigInteger(_negate(digits));
+    }
+
+    BigInteger abs() const {
+        return BigInteger(_abs(digits));
+    }
+
+    BigInteger max(const BigInteger& b) const {
         return BigInteger(_max(digits, b.toString()));
     }
 
@@ -244,8 +265,12 @@ public:
         return BigInteger(eval(digits, other.digits, BigInteger::OP::ADD));
     }
 
-    BigInteger operator-(BigInteger& other) const {
+    BigInteger operator-(const BigInteger& other) const {
         return BigInteger(eval(digits, other.digits, BigInteger::OP::SUB));
+    }
+
+    bool operator<(const BigInteger& other) const {
+        return this->compare(other) < 0;
     }
 
     bool operator==(const BigInteger& other) const {
@@ -256,7 +281,7 @@ public:
         return digits == s;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, BigInteger& bsi) {
+    friend std::ostream& operator<<(std::ostream& os, const BigInteger& bsi) {
         os << bsi.toString();
         return os;
     }
--- cpp-fundamentals/numlib/main.cpp
+++ cpp-fundamentals/numlib/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "BigInteger.cpp"
 #include <assert.h>
+#include <chrono>
 
 int main() {
 
@@ -10,66 +11,78 @@ int main() {
      * 1. compare
      */
 
-    BigInteger cmp1("1");
-    BigInteger cmp2("2");
-
-    assert(cmp1.compare(cmp2) < 0);
-    assert(cmp2.compare(cmp1) > 0);
-
-    BigInteger cmp3("-1");
-    BigInteger cmp4("1");
-
-    assert(cmp3.compare(cmp4) < 0);
-    assert(cmp4.compare(cmp3) > 0);
-
-    BigInteger cmp5("0");
-    BigInteger cmp6("1");
-
-    assert(cmp5.compare(cmp6) < 0);
-    assert(cmp6.compare(cmp5) > 0);
-
-    BigInteger cmp7("1");
-    BigInteger cmp8("1");
-
-    assert(cmp7.compare(cmp8) == 0);
-    assert(cmp8.compare(cmp7) == 0);
-
-    BigInteger cmp9("11");
-    BigInteger cmp10("15");
-
-    assert(cmp9.compare(cmp10) < 0);
-    assert(cmp10.compare(cmp9) > 0);
-
-    BigInteger cmp11("1111");
-    BigInteger cmp12("999");
-
-    assert(cmp11.compare(cmp12) > 0);
-    assert(cmp12.compare(cmp11) < 0);
-
-    BigInteger cmp13("-932");
-    BigInteger cmp14("-73");
-
-    assert(cmp13.compare(cmp14) < 0);
-    assert(cmp14.compare(cmp13) > 0);
-
-    BigInteger cmp15("-9");
-    BigInteger cmp16("-9");
-
-    assert(cmp15.compare(cmp16) == 0);
-    assert(cmp16.compare(cmp15) == 0);
-
-    BigInteger cmp17("-9873");
-    BigInteger cmp18("9283");
-
-    assert(cmp17.compare(cmp18) < 0);
-    assert(cmp18.compare(cmp17) > 0);
-
-    BigInteger cmp19("-1");
-    BigInteger cmp20("-3");
-
-    assert(cmp19.compare(cmp20) > 0);
-
-
+//    BigInteger cmp1("1");
+//    BigInteger cmp2("2");
+//
+//    assert(cmp1.compare(cmp2) < 0);
+//    assert(cmp2.compare(cmp1) > 0);
+//
+//    BigInteger cmp3("-1");
+//    BigInteger cmp4("1");
+//
+//    assert(cmp3.compare(cmp4) < 0);
+//    assert(cmp4.compare(cmp3) > 0);
+//
+//    BigInteger cmp5("0");
+//    BigInteger cmp6("1");
+//
+//    assert(cmp5.compare(cmp6) < 0);
+//    assert(cmp6.compare(cmp5) > 0);
+//
+//    BigInteger cmp7("1");
+//    BigInteger cmp8("1");
+//
+//    assert(cmp7.compare(cmp8) == 0);
+//    assert(cmp8.compare(cmp7) == 0);
+//
+//    BigInteger cmp9("11");
+//    BigInteger cmp10("15");
+//
+//    assert(cmp9.compare(cmp10) < 0);
+//    assert(cmp10.compare(cmp9) > 0);
+//
+//    BigInteger cmp11("1111");
+//    BigInteger cmp12("999");
+//
+//    assert(cmp11.compare(cmp12) > 0);
+//    assert(cmp12.compare(cmp11) < 0);
+//
+//    BigInteger cmp13("-932");
+//    BigInteger cmp14("-73");
+//
+//    assert(cmp13.compare(cmp14) < 0);
+//    assert(cmp14.compare(cmp13) > 0);
+//
+//    BigInteger cmp15("-9");
+//    BigInteger cmp16("-9");
+//
+//    assert(cmp15.compare(cmp16) == 0);
+//    assert(cmp16.compare(cmp15) == 0);
+//
+//    BigInteger cmp17("-9873");
+//    BigInteger cmp18("9283");
+//
+//    assert(cmp17.compare(cmp18) < 0);
+//    assert(cmp18.compare(cmp17) > 0);
+//
+//    BigInteger cmp19("-1");
+//    BigInteger cmp20("-3");
+//
+//    assert(cmp19.compare(cmp20) > 0);
+//
+//    /*
+//     * isNegative()
+//     */
+//    BigInteger n1("-1");
+//    assert(n1.isNegative());
+//
+//    BigInteger n2("1");
+//    assert(!n2.isNegative());
+//
+//    BigInteger n3("0");
+//    assert(!n3.isNegative());
+//
+//
 //    BigInteger a("0");
 //    BigInteger b("0");
 //
@@ -116,67 +129,67 @@ int main() {
 //    BigInteger b7("30");
 //
 //    assert((a7 - b7) == "-60");
+////
+////    // Test 'max' method
+//    BigInteger x("-1");
+//    BigInteger y("-3");
 //
-//    // Test 'max' method
-    BigInteger x("-1");
-    BigInteger y("-3");
-
-    assert(x.max(y) == "-1");
-
-    BigInteger x2("200");
-    BigInteger y2("100");
-
-    assert(x2.max(y2) == "200");
-
-    BigInteger x3("1000");
-    BigInteger y3("999");
-
-    assert(x3.max(y3) == "1000");
-
-    BigInteger x4("892");
-    BigInteger y4("982");
-
-    assert(x4.max(y4) == "982");
-
-    BigInteger x5("-3");
-    BigInteger y5("5");
-
-    assert(x5.max(y5) == "5");
-
-    BigInteger x6("5");
-    BigInteger y6("-3");
-
-    assert(x6.max(y6) == "5");
-
-    BigInteger x7("0");
-    BigInteger y7("1");
-
-    assert(x7.max(y7) == "1");
-
-    BigInteger x8("1");
-    BigInteger y8("0");
-
-    assert(x8.max(y8) == "1");
-
-    BigInteger x9("0");
-    BigInteger y9("-1");
-
-    assert(x9.max(y9) == "0");
-
-    BigInteger x10("0");
-    BigInteger y10("100");
-    BigInteger x11("100");
-    BigInteger y11("0");
-
-    assert(x10.max(y10) == y10.max(x10));
-
-    BigInteger x12("2");
-    BigInteger y12("1");
-    BigInteger z12("3");
-
-    assert(x12.max(y12).max(z12) == "3");
+//    assert(x.max(y) == "-1");
 //
-//    // Subtract
+//    BigInteger x2("200");
+//    BigInteger y2("100");
+//
+//    assert(x2.max(y2) == "200");
+//
+//    BigInteger x3("1000");
+//    BigInteger y3("999");
+//
+//    assert(x3.max(y3) == "1000");
+//
+//    BigInteger x4("892");
+//    BigInteger y4("982");
+//
+//    assert(x4.max(y4) == "982");
+//
+//    BigInteger x5("-3");
+//    BigInteger y5("5");
+//
+//    assert(x5.max(y5) == "5");
+//
+//    BigInteger x6("5");
+//    BigInteger y6("-3");
+//
+//    assert(x6.max(y6) == "5");
+//
+//    BigInteger x7("0");
+//    BigInteger y7("1");
+//
+//    assert(x7.max(y7) == "1");
+//
+//    BigInteger x8("1");
+//    BigInteger y8("0");
+//
+//    assert(x8.max(y8) == "1");
+//
+//    BigInteger x9("0");
+//    BigInteger y9("-1");
+//
+//    assert(x9.max(y9) == "0");
+//
+//    BigInteger x10("0");
+//    BigInteger y10("100");
+//    BigInteger x11("100");
+//    BigInteger y11("0");
+//
+//    assert(x10.max(y10) == y10.max(x10));
+//
+//    BigInteger x12("2");
+//    BigInteger y12("1");
+//    BigInteger z12("3");
+//
+//    assert(x12.max(y12).max(z12) == "3");
+////
+////    // Subtract
 //    BigInteger as("30");
 //    BigInteger bs("10");
 //
@@ -199,10 +212,27 @@ int main() {
 //
 //    // https://www.calculator.net/big-number-calculator.html was used to verify this
 //    assert(as3 - bs3 == "440894055503451850309096488199908550346494");
-
+//
 //    BigInteger big1("-999999933333333333388888888888888888444444444444999999933333333333388888888888888888444444444444999999933333333333388888888888888888444444444444");
 //    BigInteger big2("293829839289382938928392839289328938928398293829839289382938928392839289328938928398293829839289382938928392839289328938928398293829839289382938928392839289328938928398");
 //
 //    assert(big1 + big2 == "293829839289382938928391839289395605595064904940950400494050483948394844328938995064960496450400494050039504394844884493928398360496505955994050039503950400884494483954");
+
+    typedef std::chrono::high_resolution_clock clock;
+    typedef std::chrono::duration<float, std::milli> mil;
+
+    auto t = clock::now();
+
+    for (int i = 0; i < 1000000; i++) {
+        BigInteger x(std::to_string(i));
+        BigInteger y(std::to_string(i + 1));
+
+        BigInteger z = x + y;
+    }
+
+    auto tprime = clock::now();
+
+    std::cout << mil(tprime - t).count() << " ms." << std::endl;
+
     return 0;
 }
